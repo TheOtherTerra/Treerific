4494088d7942f92bd9b5c88d5f1b6d8c9a0f78d2
Terra_XYZ <terranova.yessir@gmail.com>
2025-10-29 09:37:53 -0400

    Rename class and mod ID from TerraEclipse to Treerific
    -REMOTE

--- src/main/java/net/terraxyz/treerific/Treerific.java
+++ src/main/java/net/terraxyz/treerific/Treerific.java
@@ -23,8 +23,8 @@ import net.terraxyz.terraeclipse.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TerraEclipse.MOD_ID)
-public class TerraEclipse {
+@Mod(Treerific.MOD_ID)
+public class Treerific {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "treerific";
     // Directly reference a slf4j logger
